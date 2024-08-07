24e8269b4aadd3b62a02ee1a8cb5b45e7dd48585
med <mohamedelafia732@gmail.com>
2024-08-07 07:42:43 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/disease-predection-frontend/",
+  build: {
+    outDir: "dist",
+},
 })
